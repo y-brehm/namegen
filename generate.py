678f1071a068ccd32113b38015a7afee9f700273
Simon <simon.vincente.zimmermann@gmail.com>
2020-10-02 21:30:53 +0200

    adding filtering

--- generate.py
+++ generate.py
@@ -1,31 +1,41 @@
 #!/usr/bin/env python
 
-from __future__ import print_function
-
 import argparse
 import os
 import wikipedia
 
-parser = argparse.ArgumentParser(
-    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+from duckduckpy import query
+
+
+def generateDataset(pageName):
+    names = wikipedia.page(pageName).links
+    datasetFile = open("char-rnn-tensorflow/data/tinyshakespeare/input.txt", "w")
+    datasetFile.truncate(0)
+    for name in names:
+        if maxNameLength >= len(name) > minNameLength:
+           datasetFile.write(f"{name}\n")
+    datasetFile.close()
+
+
+def filterResults(names):
+    outputFile = open("results.txt", 'w')
+    for name in names:
+        response = query(name, container='dict')
+        if not response['heading']:
+            outputFile.write(f"{name}\n")
+
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('--page', type=str, default='List of legendary creatures from Japan',
                     help='wikipedia page to use as inspiration for name generating')
 
 args = parser.parse_args()
-
 maxNameLength = 10
 minNameLength = 3
 
-names = wikipedia.page(args.page).links
-
-datasetFile = open("char-rnn-tensorflow/data/tinyshakespeare/input.txt", "w")
-datasetFile.truncate(0)
-for name in names:
-    if maxNameLength >= len(name) > minNameLength:
-       datasetFile.write(f"{name}\n")
-datasetFile.close()
-
+generateDataset(args.page)
 os.chdir("char-rnn-tensorflow")
 os.system(f"python3 train.py --seq_length={minNameLength} --batch_size=128")
+names = os.popen('python3 sample.py').read().split()
 
-output = os.popen('python3 sample.py').read().split()
+filterResults(names)
